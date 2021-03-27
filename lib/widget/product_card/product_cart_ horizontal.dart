ad4ee7839e37b812cc1b08befb811376ea1183c7
Islam Ahmed <ra.work99@gmail.com>
2021-03-27 14:38:12 +0200

    Home

--- lib/widget/product_card/product_cart_ horizontal.dart
+++ lib/widget/product_card/product_cart_ horizontal.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import '../../helpers/base_url.dart' as baseurl;
 
 class ProductCardHorizental extends StatelessWidget {
   const ProductCardHorizental({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Container(
-         decoration: BoxDecoration(image:  DecorationImage(
-                        image: NetworkImage(), fit: BoxFit.cover)),),
-              
-             
-          )
-        ],
+    return Scaffold(
+      body: Card(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                  image: NetworkImage(
+                    baseurl.Urls.public_api +
+                        '/uploads/all/EUVhpp6F3hBgCpjCd6NoZGmtHv5lhEyPeNG6Jq9g.jpeg',
+                  ),
+                  fit: BoxFit.cover,
+                  onError: (o, e) {
+                    print(o);
+                  },
+                )),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
