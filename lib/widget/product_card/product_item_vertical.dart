4a10cb94e12eef09ea9035ae755c0463f43a435c
Islam Ahmed <ra.work99@gmail.com>
2021-03-28 19:42:07 +0200

    Home BestSelling cat

--- lib/widget/product_card/product_item_vertical.dart
+++ lib/widget/product_card/product_item_vertical.dart
@@ -21,77 +21,72 @@ class ProductItemVertical extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      child: Row(
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            //height: MediaQuery.of(context).size.height * 0.2,
-            //width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Expanded(
+            flex: 1,
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: NetworkImage(baseurl.Urls.public_api + '/' + image),
+                fit: BoxFit.fill,
+                onError: (o, e) {
+                  print(o);
+                },
+              )),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Container(
-                          height: 100,
-                          width: 100,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                            image: NetworkImage(
-                                baseurl.Urls.public_api + '/' + image),
-                            fit: BoxFit.fill,
-                            onError: (o, e) {
-                              print(o);
-                            },
+                Container(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Flexible(
+                      flex: 2,
+                      child: Text(name,
+                          //   textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
                           )),
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Text(name,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  // fontSize: 16,
-                                  fontWeight: FontWeight.w800)),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Rating(itemSize: 20, initialRating: rating),
-                        ],
-                      ),
-                    ],
-                  ),
+                    )
+                  ],
+                )),
+                SizedBox(
+                  height: 20,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(unitPrice.toString(),
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w800)),
-                        Text(unitPrice2.toString(),
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w800)),
-                        Text(unitPrice3.toString(),
-                            style: TextStyle(
-                                color: Theme.of(context).primaryColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w800)),
-                      ],
-                    ),
-                  ),
-                )
+                Rating(itemSize: 20, initialRating: rating),
               ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(unitPrice.toString(),
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800)),
+                  Text(unitPrice2.toString(),
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800)),
+                  Text(unitPrice3.toString(),
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w800)),
+                ],
+              ),
             ),
           ),
         ],
