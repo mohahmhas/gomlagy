e06f49b0cdd11e682086782e1215b61a2a9b48a2
MostafaMohamedMokhtar <mokhtarmostafa13@gmail.com>
2021-03-29 15:39:34 +0200

    use

--- lib/screens/naviation_bottom/user_navigation.dart
+++ lib/screens/naviation_bottom/user_navigation.dart
@@ -8,96 +8,44 @@ class UserNav extends StatelessWidget {
         top: 130,
       ),
       child: Container(
-          color: Colors.deepPurple[100],
+          padding: EdgeInsets.all(6),
           height: MediaQuery.of(context).size.height - 180,
           child: ListView(
             children: [
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
-              ListTile(
-                title: Text('Home'),
-                subtitle: Text('Home dfdfhdghdghdghdghdghdg'),
-                leading: Icon(Icons.ac_unit),
-                trailing: Icon(Icons.ac_unit),
-              ),
+              viewListTile('Wishlist',Icons.thumb_up_alt_outlined) ,
+              SizedBox(height: 10,) ,
+              viewListTile('Purchase History',Icons.receipt_long_rounded) ,
+              SizedBox(height: 10,) ,
+              viewListTile('My Wallet',Icons.wallet_travel) ,
+              SizedBox(height: 10,) ,
+              viewListTile('Account Information',Icons.info_outline) ,
+              SizedBox(height: 10,) ,
+              viewListTile('Warrantly',Icons.person) ,
+              SizedBox(height: 10,) ,
+              viewListTile("Language",Icons.language_outlined ) ,
+              SizedBox(height: 10,) ,
+              viewListTile('Join as a trader',Icons.attach_money_outlined ) ,
+              SizedBox(height: 10,) ,
+              viewListTile('Logout',Icons.logout ) ,
+
+              
             ],
           )),
     );
+  }
+
+
+  Widget viewListTile (String name , IconData icon){
+
+            return  GestureDetector(
+              onTap: (){
+
+              },
+              child: ListTile(
+                  title: Text(name),
+                  leading: Icon(icon),
+                  trailing: Icon(Icons.arrow_forward_ios)
+                ),
+            );
   }
 }
