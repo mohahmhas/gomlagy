8eff6e5c3f89aa73c381b638f2a84fe0f5ec537b
MostafaMohamedMokhtar <mokhtarmostafa13@gmail.com>
2021-03-29 15:40:05 +0200

    use

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
           textStyle: TextStyle(
             fontSize: 30.0,
           ),
-          backgroundColor: Colors.orange[900],
+          backgroundColor: Color(0xffeb8624),
         ),
       ),
     );
